2046296e8764a2700aacafa88ae102036d37f09c
rfalaize <rhome.falaize@gmail.com>
2020-05-23 22:49:25 +0800

    remove warning

---
+++ web/deploy-firebase.cmd
@@ -0,0 +1,7 @@
+@echo off
+
+echo ***************************************************************
+echo Start deployment on firebase...
+echo ***************************************************************
+
+firebase deploy && echo "Done! Press any key to return..." && pause
