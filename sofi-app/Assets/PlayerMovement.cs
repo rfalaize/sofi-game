1bca51e4191ca106c74a5abc086aca179c87a686
rfalaize <rhome.falaize@gmail.com>
2020-05-24 19:03:52 +0800

    add player movement and audio

---
+++ sofi-app/Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float runSpeed = 40f;
+
+    private float horizontalMove = 0f;
+    private bool jump = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // store movement inputs
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        jump = Input.GetAxisRaw("Vertical") > 0 ? true : false;
+    }
+
+    void FixedUpdate()
+    {
+        // move character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
