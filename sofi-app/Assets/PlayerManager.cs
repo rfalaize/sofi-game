82d8746d90b106a5adc51da071de15ea99173688
rfalaize <rhome.falaize@gmail.com>
2020-06-06 13:26:44 +0800

    add joystick

--- sofi-app/Assets/PlayerManager.cs
+++ sofi-app/Assets/PlayerManager.cs
@@ -2,6 +2,7 @@
 
 public class PlayerManager : MonoBehaviour
 {
+    public Joystick joystick;
     public CharacterController2D controller;
     public float runSpeed = 40f;
 
@@ -12,19 +13,12 @@ public class PlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // store movement inputs
-        if (Input.touchCount > 0)
-        {
-            // touch screen inputs
-
-
-        } else
-        {
-            // keyboard inputs
-            horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-            jump = Input.GetAxisRaw("Vertical") > 0 ? true : false;
-            crouch = Input.GetAxisRaw("Vertical") < 0 ? true : false;
-        }
+        // get inputs from joystick or keyboard
+        float horizontalInput = (System.Math.Abs(joystick.Horizontal) > 0.2) ? joystick.Horizontal : Input.GetAxisRaw("Horizontal");
+        float verticalInput = (joystick.Vertical >= 0.8) ? joystick.Vertical : Input.GetAxisRaw("Vertical");
+        horizontalMove = horizontalInput * runSpeed;
+        jump = verticalInput > 0 ? true : false;
+        // crouch = verticalInput < 0 ? true : false;
     }
 
     void FixedUpdate()
